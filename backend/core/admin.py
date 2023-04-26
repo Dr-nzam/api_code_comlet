e9315165f4cc4bda74b3e3b32232697611a1aee3
Dr-nzam <nzambouarmel@gmail.com>
2023-04-26 13:03:45 +0100

    le model est bon

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+admin.site.register(Cours)
+admin.site.register(Epreuves)
+admin.site.register(Citations)
+admin.site.register(Utilisateurs)
+admin.site.register(Preoccupation)
+admin.site.register(Reponse)
+admin.site.register(Competition)
+admin.site.register(ReponseCompetition)
--- backend/core/models.py
+++ backend/core/models.py
@@ -29,46 +29,46 @@ class Utilisateurs (models.Model):
     nom = models.CharField(max_length=128)
     niveauR = models.CharField(max_length=128)
     filiere = models.CharField(max_length=128)
-    telephone = models.IntegerField(max_length=15)
+    telephone = models.IntegerField()
     matiere_dispenserR = models.CharField(max_length=200)
-    classeE = models.CharField(max_length=128, choices=['3eme', '2nd','premier','terminal'], default='')
+    classeE = models.CharField(max_length=128, choices=(('3eme','3eme'), ('2nd','2nd'),('premier','premier'),('terminal','terminal')), default='')
     etablissement = models.CharField(max_length=128, blank=True)
     password = models.CharField(max_length=128)
-    salaireR = models.IntegerField(max_length=128, default=0)
+    salaireR = models.IntegerField(default=0)
     pays = models.CharField(max_length=128)
     ville = models.CharField(max_length=128,)
     quatier = models.CharField(max_length=128)
-    role = models.CharField(default='eleve', choices=['eleve','repetiteur'], max_length=128)
+    role = models.CharField(default='eleve', choices=(('eleve','eleve'),('repetiteur','repetiteur')), max_length=128)
     
     
     
 class Preoccupation (models.Model):
-    auteur = models.ForeignKey(Utilisateurs, on_delete=models.CASCADE)
+    auteur = models.ForeignKey(Utilisateurs, on_delete=models.CASCADE,related_name='mocta1')
     contenue = models.CharField(max_length=128)
     fichier = models.FileField(upload_to='document/preoccupation', blank=True,)
-    classe = models.ForeignKey(Utilisateurs, on_delete=models.CASCADE)
+    classe = models.ForeignKey(Utilisateurs, on_delete=models.CASCADE,related_name='mocta2')
     
 
 class Reponse (models.Model):
     auteur = models.ForeignKey(Utilisateurs, on_delete=models.CASCADE)
     contenue = models.CharField(max_length=128)
     fichier = models.FileField(upload_to='document/reponse', blank=True,)
-    classe = models.ForeignKey(Utilisateurs, on_delete=models.CASCADE)
-    poste = models.ForeignKey(Utilisateurs, on_delete=models.CASCADE)
+    classe = models.ForeignKey(Utilisateurs, on_delete=models.CASCADE,related_name='mocta3')
+    poste = models.ForeignKey(Utilisateurs, on_delete=models.CASCADE,related_name='mocta4')
     
 class Competition (models.Model):
     auteur = models.CharField(max_length=128)
     contenue = models.CharField(max_length=128)
     fichier = models.FileField(upload_to='document/competition', blank=True,)
-    classe = models.CharField(max_length=128, choices=['3eme', '2nd','premier','terminal'], default='')
+    classe = models.CharField(max_length=128, choices=(('3eme','3eme'), ('2nd','2nd'),('premier','premier'),('terminal','terminal')), default='')
     date_debut_Competion = models.DateField()
     date_fin_Competion = models.DateField()
-    poste = models.ForeignKey(default='MOCTA-EDUCATION', max_length=128)
+    poste = models.CharField(default='MOCTA-EDUCATION', max_length=128)
     
     
 class ReponseCompetition (models.Model):
       
-    eleve = models.ForeignKey(Utilisateurs, on_delete=models.CASCADE)
+    eleve = models.ForeignKey(Utilisateurs, on_delete=models.CASCADE,related_name='mocta5')
     reponse1 = models.TextField()
     reponse2 = models.FileField(upload_to='document/reponse/reponse_competition')
        
