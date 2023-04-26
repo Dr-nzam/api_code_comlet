798f0942e6a464e06f72476630bbed0b7ae7421e
Dr-nzam <nzambouarmel@gmail.com>
2023-04-26 10:03:02 +0100

    p4

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Cour (models.Model):
+    nom = models.CharField(max_length=128)
+    titre = models.CharField(max_length=128, blank= True, default='bon à savoir')
+    contenue = models.TextField|()
+    fichier = models.FileField(upload_to='document/')
