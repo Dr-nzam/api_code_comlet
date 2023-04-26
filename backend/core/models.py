4411d7bb3d68a8c494a396d0b67870df1e4dbbd6
Dr-nzam <nzambouarmel@gmail.com>
2023-04-26 11:18:55 +0100

    p7

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,49 +1,56 @@
 from django.db import models
 
 # Create your models here.
-class Cour (models.Model):
+class Cours (models.Model):
     nom = models.CharField(max_length=128)
     titre = models.CharField(max_length=128, blank= True, default='bon à savoir')
     contenue = models.TextField|()
     fichier = models.FileField(upload_to='document/')
     
 
-class epreuve (models.Model):
+class Epreuves (models.Model):
     nom = models.CharField(max_length=128)
     titre = models.CharField(max_length=128, blank= True, default='bon à savoir')
     fichier = models.FileField(upload_to='document/')
     
     
-class citations (models.Model):
+class Citations (models.Model):
     contenue = models.CharField(max_length=128)
     auteur = models.CharField(max_length=128, blank= True, default='anonyme')
+        
     
 
-class citations (models.Model):
-    nom = models.CharField(max_length=128)
-    niveau = models.CharField(max_length=128)
-    telephone = models.IntegerField(max_length=15)
-    matiere = models.CharField(max_length=200)
-    pays = models.CharField(max_length=128)
-    ville = models.CharField(max_length=128)
-    quatier = models.CharField(max_length=128)
-    salaire = models.IntegerField(max_length=128, default=0)
+class Utilisateurs (models.Model):
+    # Les champs avec le R à la fin sont les champs destinée au repetiteur uniquement 
+    # Les champs avec le E à la fin sont les champs destinée au eleve  uniquement
+    # Le reste de champs le sont commun 
     
-
-class eleve (models.Model):
     nom = models.CharField(max_length=128)
-    telephone = models.IntegerField(max_length=15)
-    classe = models.CharField(max_length=128)
-    password = models.CharField(max_length=128)
+    niveauR = models.CharField(max_length=128)
     filiere = models.CharField(max_length=128)
+    telephone = models.IntegerField(max_length=15)
+    matiere_dispenserR = models.CharField(max_length=200)
+    classeE = models.CharField(max_length=128, choices=['3eme', '2nd','premier','terminal'], default='')
+    password = models.CharField(max_length=128)
+    salaireR = models.IntegerField(max_length=128, default=0)
     pays = models.CharField(max_length=128)
-    ville = models.CharField(max_length=128)
+    ville = models.CharField(max_length=128,)
     quatier = models.CharField(max_length=128)
+    role = models.CharField(default='eleve', choices=['eleve','repetiteur'], max_length=128)
     
-class preoccupation (models.Model):
-    auteur = models.CharField(max_length=128, blank= True, default='anonyme')
+    
+class Preoccupation (models.Model):
+    auteur = models.ForeignKey(Utilisateurs, on_delete=models.CASCADE)
     contenue = models.CharField(max_length=128)
     fichier = models.FileField(upload_to='document/', blank=True,)
+    classe = models.ForeignKey(Utilisateurs, on_delete=models.CASCADE)
     
+
+class Reponse (models.Model):
+    auteur = models.ForeignKey(Utilisateur, on_delete=models.CASCADE)
+    contenue = models.CharField(max_length=128)
+    fichier = models.FileField(upload_to='document/', blank=True,)
+    classe = models.ForeignKey(Utilisateur, on_delete=models.CASCADE)
+    poste = models.ForeignKey(Utilisateur, on_delete=models.CASCADE)
     
   
