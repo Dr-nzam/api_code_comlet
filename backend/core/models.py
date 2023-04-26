b5c87e593a96f7ebf47d6808be0cc3cbcb1ce876
Dr-nzam <nzambouarmel@gmail.com>
2023-04-26 10:23:01 +0100

    p5

--- backend/core/models.py
+++ backend/core/models.py
@@ -6,3 +6,37 @@ class Cour (models.Model):
     titre = models.CharField(max_length=128, blank= True, default='bon à savoir')
     contenue = models.TextField|()
     fichier = models.FileField(upload_to='document/')
+    
+
+class epreuve (models.Model):
+    nom = models.CharField(max_length=128)
+    titre = models.CharField(max_length=128, blank= True, default='bon à savoir')
+    fichier = models.FileField(upload_to='document/')
+    
+    
+class citations (models.Model):
+    contenue = models.CharField(max_length=128)
+    auteur = models.CharField(max_length=128, blank= True, default='bon à savoir')
+    
+
+class citations (models.Model):
+    nom = models.CharField(max_length=128)
+    niveau = models.CharField(max_length=128)
+    telephone = models.IntegerField(max_length=15)
+    matiere = models.CharField(max_length=200)
+    pays = models.CharField(max_length=128)
+    ville = models.CharField(max_length=128)
+    quatier = models.CharField(max_length=128)
+    salaire = models.IntegerField(max_length=128, default=0)
+    
+
+class eleve (models.Model):
+    nom = models.CharField(max_length=128)
+    telephone = models.IntegerField(max_length=15)
+    classe = models.CharField(max_length=128)
+    filiere = models.CharField(max_length=128)
+    pays = models.CharField(max_length=128)
+    ville = models.CharField(max_length=128)
+    quatier = models.CharField(max_length=128)
+    
+  
